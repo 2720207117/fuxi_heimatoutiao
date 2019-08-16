ece22c42f57a126938eb28a130b07c8d001720d1
CJX2720207117 <2720207117@qq.com>
2019-08-16 19:45:49 +0800

    登录模块-基础布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    APP组件
-    <el-button type="primary">普通按钮</el-button>
     <router-view></router-view>
   </div>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="app">
-        Login组件
+        <el-card class="card">
+            <img src="../../assets/images/logo_index.png" alt="logo">
+
+        </el-card>
     </div>
 </template>
 
@@ -11,5 +14,27 @@ export default {
 </script>
 
 <style lang="less" scoped>
+    #app {
+        background: url('../../assets/images/login_bg.jpg') no-repeat center / cover;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
 
+    .card {
+        width: 420px;
+        height: 360px;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+
+        img {
+            display: block;
+            width: 240px;
+            margin: 0 auto;
+        }
+    }
 </style>
