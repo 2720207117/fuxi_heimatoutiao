f0439e46d48ce99da700dad5e1fb72353a3bf78e
CJX2720207117 <2720207117@qq.com>
2019-08-16 17:52:34 +0800

    登录模块-路由及组件创建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     APP组件
     <el-button type="primary">普通按钮</el-button>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,14 @@
 
 import VueRouter from 'vue-router' // 导包(vue-router)
 import Vue from 'vue'
+
+import Login from '@/views/login' // 导入login组件
+
 Vue.use(VueRouter) // 在全局使用
 
 const router = new VueRouter({
   routes: [
-
+    { name: 'login', path: '/login', component: Login }
   ]
 })
 export default router
