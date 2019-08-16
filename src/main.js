47bd8e6a253d70a0bd6fea1b61767c6941b8befe
CJX2720207117 <2720207117@qq.com>
2019-08-16 17:33:56 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -6,10 +6,16 @@ import App from './App.vue'
 
 import ElementUI from 'element-ui' // 导包(element-ui)
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+
+// 在vue-cli创建的项目中，@ 前戳 代表的是根路径 src目录
+// 默认会加载索引路径 可不用去写 （索引文件：index.js index.json）
+import router from '@/router'
+
 Vue.use(ElementUI) // 在全局范围注册组件库
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router: router // 挂载跟实例
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,12 @@
 // 创建路由对象 及 配置路由规则 和 其他路由配置
+
+import VueRouter from 'vue-router' // 导包(vue-router)
+import Vue from 'vue'
+Vue.use(VueRouter) // 在全局使用
+
+const router = new VueRouter({
+  routes: [
+
+  ]
+})
+export default router
