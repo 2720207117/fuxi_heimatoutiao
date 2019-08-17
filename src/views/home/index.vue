4d34b64fe527a2390720054cb201937607c17351
CJX2720207117 <2720207117@qq.com>
2019-08-17 17:33:18 +0800

    首页模块-基础布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="app">
         Home组件
+        <el-container class="home-container">
+            <el-aside width="200px">Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
@@ -10,5 +17,22 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-
+    .home-container {
+        background:rebeccapurple;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+    .el-aside {
+        height: 100%;
+        background: #ccc;
+    }
+    .el-header {
+        background: rgb(57, 248, 32);
+    }
+    .el-main {
+        background: red;
+    }
 </style>
