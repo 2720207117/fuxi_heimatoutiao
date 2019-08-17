2981b4e9c39a86a0ca780d97f209543817df5d8c
CJX2720207117 <2720207117@qq.com>
2019-08-17 19:41:29 +0800

    首页模块-基础布局-头部

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,7 +4,22 @@
         <el-container class="home-container">
             <el-aside width="200px">Aside</el-aside>
             <el-container>
-                <el-header>Header</el-header>
+                <el-header class="header">
+                    <span class="el-icon-s-fold"></span>
+                    <span class="text">江苏传智播客教育科技有限公司</span>
+                    <span class="user">
+                        <img src="../../assets/images/avatar.jpg" alt="头像">
+                        <el-dropdown trigger="click">
+                            <span class="el-dropdown-link">
+                                下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+                            </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+                                <el-dropdown-item icon="el-icon-lock">退出登录</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </span>
+                </el-header>
                 <el-main>Main</el-main>
             </el-container>
         </el-container>
@@ -18,7 +33,6 @@ export default {
 </script>
 <style lang="less" scoped>
     .home-container {
-        background:rebeccapurple;
         width: 100%;
         height: 100%;
         position: absolute;
@@ -29,10 +43,25 @@ export default {
         height: 100%;
         background: #ccc;
     }
-    .el-header {
-        background: rgb(57, 248, 32);
-    }
-    .el-main {
-        background: red;
+    .header {
+        border-bottom: 1px solid #ccc;
+        box-shadow: 0px 0px 5px 0px rgb(179, 177, 177);
+        line-height: 60px;
+        .el-icon-s-fold {
+            font-size: 26px;
+            vertical-align: middle;
+            margin-right: 10px;
+        }
+        .text {
+            vertical-align: middle;
+        }
+        .user {
+            float: right;
+            img {
+                vertical-align: middle;
+                width: 30px;
+                margin-right: 6px;
+            }
+        }
     }
 </style>
