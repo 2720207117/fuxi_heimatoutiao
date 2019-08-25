e92c6b71012d4bf6e4cb753390fd0ebf56f45784
CJX2720207117 <2720207117@qq.com>
2019-08-25 16:50:50 +0800

    内容管理-频道组件封装

---
+++ src/components/my-channel.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <el-select :value="value" @change="fn" placeholder="请选择">
+            <el-option
+            v-for="item in channelOptions"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
+            </el-option>
+        </el-select>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'my-channel',
+  props: ['value'],
+  data () {
+    return {
+      channelOptions: []
+    }
+  },
+  created () {
+    // 获取频道数据
+    this.getChannelOptions()
+  },
+  methods: {
+    async getChannelOptions () {
+      const { data: { data } } = await this.$http.get('channels')
+      this.channelOptions = data.channels
+      this.$emit('input', data.channels.id)
+    },
+    fn (value) { // 下拉菜单 change事件中 默认会有一个参数：被选中的值
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,95 +1,97 @@
 <template>
-    <div id="app">
-        <el-card>
-            <div slot="header">
-                <my-bread>内容管理</my-bread>
-            </div>
-                <el-form :model="reqParams" size="small" label-width="80px">
-                    <el-form-item label="状态：">
-                        <el-radio-group v-model="reqParams.status">
-                            <el-radio :label="null">全部</el-radio>
-                            <el-radio :label="0">草稿</el-radio>
-                            <el-radio :label="1">待审核</el-radio>
-                            <el-radio :label="2">审核通过</el-radio>
-                            <el-radio :label="3">审核失败</el-radio>
-                        </el-radio-group>
-                    </el-form-item>
-                    <el-form-item label="频道：">
-                        <el-select v-model="reqParams.channel_id" placeholder="请选择">
-                            <el-option
-                            v-for="item in channelOptions"
-                            :key="item.id"
-                            :label="item.name"
-                            :value="item.id">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item label="时间：">
-                        <el-date-picker
-                            value-format="yyyy-MM-dd"
-                            @change="changeDate"
-                            v-model="dateValues"
-                            type="daterange"
-                            range-separator="至"
-                            start-placeholder="开始日期"
-                            end-placeholder="结束日期">
-                        </el-date-picker>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="search()">筛选</el-button>
-                    </el-form-item>
-                </el-form>
-        </el-card>
-        <el-card>
-            <div slot="header">
-                根据筛选条件共查询到 <b>{{total_count}}</b> 条结果：
-            </div>
-            <el-table :data="tableData" style="width: 100%">
-                <el-table-column label="封面">
-                    <template slot-scope="scope">
-                        <el-image :src="scope.row.cover.images[0]" alt="" lazy style="width: 100px; height: 75px">
-                            <div slot="error" class="image-slot">
-                                <img src="../../assets/images/error.gif" alt="" width="100px" height="75px">
-                            </div>
-                        </el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column label="标题" prop="title">
-                    <!-- <template slot-scope="scope">
-                        {{scope.row.id}}  此处目的：显示文章id 被转换的是否准确，以便用于删除文章数据
-                    </template> -->
-                </el-table-column>
-                <el-table-column label="状态">
-                    <template slot-scope="scope">
-                        <el-tag v-if="scope.row.status == 0" type="info">草稿</el-tag>
-                        <el-tag v-if="scope.row.status == 1">待审核</el-tag>
-                        <el-tag v-if="scope.row.status == 2" type="success">审核通过</el-tag>
-                        <el-tag v-if="scope.row.status == 3" type="warning">审核失败</el-tag>
-                        <el-tag v-if="scope.row.status == 4" type="danger">已删除</el-tag>
-                    </template>
-                </el-table-column>
-                <el-table-column label="发布时间" prop="pubdate">
-                </el-table-column>
-                <el-table-column label="操作" width="120px">
-                    <template slot-scope="scope">
-                      <el-button @click="editValues(scope.row.id)" class="el-icon-edit" plain circle type="primary"></el-button>
-                    <el-button @click="deleteValues(scope.row.id)" class="el-icon-delete" plain circle type="danger"></el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
-            <!-- 分页器 -->
-            <!-- :@current-change 页码切换时触发的事件；:current-page 当前页；
-                 :page-size 一页显示多少条数据；:total 数据总条数 -->
-            <el-pagination
-                background
-                layout="prev, pager, next"
-                :total="this.total_count"
-                :page-size="reqParams.per_page"
-                :current-page="reqParams.page"
-                @current-change="changePager">
-            </el-pagination>
-        </el-card>
-    </div>
+  <div id="app">
+    <el-card>
+      <div slot="header">
+        <my-bread>内容管理</my-bread>
+      </div>
+      <el-form :model="reqParams" size="small" label-width="80px">
+        <el-form-item label="状态：">
+          <el-radio-group v-model="reqParams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <!-- v-model 本质：v-bind:value=""  v-on:input="" -->
+          <my-channel v-model="reqParams.channel_id"></my-channel>
+          <!-- <el-select placeholder="请选择">
+            <el-option
+            v-for="item in channelOptions"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
+            </el-option>
+          </el-select> -->
+        </el-form-item>
+        <el-form-item label="时间：">
+          <el-date-picker
+            value-format="yyyy-MM-dd"
+            @change="changeDate"
+            v-model="dateValues"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="search()">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card>
+      <div slot="header">
+        根据筛选条件共查询到 <b>{{total_count}}</b> 条结果：
+      </div>
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column label="封面">
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" alt="" lazy style="width: 100px; height: 75px">
+              <div slot="error" class="image-slot">
+                <img src="../../assets/images/error.gif" alt="" width="100px" height="75px">
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title">
+          <!-- <template slot-scope="scope">
+                {{scope.row.id}}  此处目的：显示文章id 被转换的是否准确，以便用于删除文章数据
+              </template> -->
+        </el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status == 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status == 1">待审核</el-tag>
+            <el-tag v-if="scope.row.status == 2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status == 3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status == 4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate">
+        </el-table-column>
+        <el-table-column label="操作" width="120px">
+          <template slot-scope="scope">
+            <el-button @click="editValues(scope.row.id)" class="el-icon-edit" plain circle type="primary"></el-button>
+            <el-button @click="deleteValues(scope.row.id)" class="el-icon-delete" plain circle type="danger"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页器 -->
+      <!-- :@current-change 页码切换时触发的事件；:current-page 当前页；
+            :page-size 一页显示多少条数据；:total 数据总条数 -->
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="this.total_count"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="changePager">
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -110,7 +112,7 @@ export default {
         begin_pubdate: null,
         end_pubdate: null
       },
-      channelOptions: [],
+      // channelOptions: [],
       dateValues: [],
       tableData: [],
       total_count: 0 // 定义数据总条数
@@ -118,24 +120,24 @@ export default {
   },
   created () {
     // 获取频道数据
-    this.getChannelOptions()
+    // this.getChannelOptions()
 
     // 获取文章列表数据
     this.gettableData()
   },
   methods: {
     // 获取频道数据的方法
-    async getChannelOptions () {
-      const { data: { data } } = await this.$http.get('channels')
-      this.channelOptions = data.channels
-    },
+    // async getChannelOptions () {
+    //   const { data: { data } } = await this.$http.get('channels')
+    //   this.channelOptions = data.channels
+    // },
 
     // 获取文章列表数据方法
     async gettableData () {
       // post传参   axios.post('url', {参数对象})
       // get传参    axios.get('url', {params: {参数对象}})
       const { data: { data } } = await this.$http.get('articles', { params: this.reqParams })
-      console.log(data)
+      // console.log(data)
       this.total_count = data.total_count // 获取数据总条数
       this.tableData = data.results // 获取文章列表
       this.per_page = data.per_page // 获取每页数据条数
