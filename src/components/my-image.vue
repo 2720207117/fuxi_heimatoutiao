605cba5a4c02efe9171f1dc6beef6ecd930d60ae
CJX2720207117 <2720207117@qq.com>
2019-08-30 17:49:47 +0800

    发布文章-封面组件-逻辑实现(素材列表渲染 列表分页 列表切换)

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -1,31 +1,34 @@
 <template>
     <div id="app">
-        <div class="upImg" @click="dialogVisible = true">
+        <div class="upImg" @click="openDialog">
             <img src="../assets/images/default.png" alt="">
         </div>
         <!-- 对话框 -->
         <el-dialog :visible.sync="dialogVisible" width="700px" height="500px" center>
             <!-- 内容 -->
-            <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+            <el-tabs v-model="activeName" type="card" v-loading="loading">
                 <el-tab-pane label="素材库" name="images">
                     <!-- 单选按钮 -->
                     <div>
-                        <el-radio-group v-model="reqParams.collect" size="small">
+                        <el-radio-group @change="toggleImage" v-model="reqParams.collect" size="small">
                             <el-radio-button :label="false">全部</el-radio-button>
                             <el-radio-button :label="true">收藏</el-radio-button>
                         </el-radio-group>
                     </div>
                     <!-- 素材列表 -->
-                    <div class="imgBox" v-for="item in 8" :key="item">
-                        <img src="../assets/images/avatar.jpg" alt="">
+                    <div class="imgBox" v-for="item in images" :key="item.id">
+                        <img :src="item.url" alt="">
                     </div>
                     <!-- 分页器 -->
                     <el-pagination
+                        v-if="total_count > reqParams.per_page"
                         background
                         layout="prev, pager, next"
-                        :total="1000"
-                        :page-size="8"
-                        :current-page="1">
+                        :total="total_count"
+                        :page-size="reqParams.per_page"
+                        :current-page="reqParams.page"
+                        @current-change="peger"
+                        >
                     </el-pagination>
                 </el-tab-pane>
                 <el-tab-pane label="上传图片" name="upimgs">
@@ -54,6 +57,9 @@ export default {
   name: 'my-image',
   data () {
     return {
+      // 加载中
+      loading: false,
+
       // 控制对话框显示/隐藏
       dialogVisible: false,
 
@@ -69,12 +75,42 @@ export default {
 
       total_count: '', // 数据总条数
 
-      imageUrl: null // 上传图片预览地址
+      imageUrl: null, // 上传图片预览地址
+
+      images: [] // 图片素材数据
     }
   },
   methods: {
-    handleClick (tab, event) {
-      console.log(tab, event)
+    // 当点击添加素材图片按钮时，触发此函数
+    openDialog () {
+      // 打开对话框
+      this.dialogVisible = true
+
+      // 获取图片素材数据
+      this.getImages()
+    },
+
+    // 获取图片素材数据 (当点击添加素材图片按钮时，会触发此函数)
+    async getImages () {
+      this.loading = true
+      const { data: { data } } = await this.$http.get('user/images', { params: this.reqParams })
+      console.log(data)
+      this.images = data.results // 获取素材图片数据
+      this.total_count = data.total_count // 获取数据总条数
+      this.loading = false
+    },
+
+    // 当 当前页改变(点击分页按钮时)，触发此函数
+    peger (value) { // 自动传入一个参数 接收当前页
+      this.reqParams.page = value // 更新当前页
+      this.getImages() // 重新获取数据
+    },
+
+    // 当单选按钮值发生改变时，触发此函数
+    toggleImage () {
+      // 获取收藏的素材图片数据
+      this.reqParams.page = 1 // 显示第一页的请求数据
+      this.getImages()
     }
   }
 }
