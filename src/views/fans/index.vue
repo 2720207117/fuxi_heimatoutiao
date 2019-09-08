534faae2acd2a29ce4ec0e32b23a58673b984606
CJX2720207117 <2720207117@qq.com>
2019-09-08 21:41:47 +0800

    粉丝管理-echarts使用

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -8,18 +8,64 @@
                 <el-tab-pane label="粉丝列表" name="list">
                     <img style="width: 120px" src="../../assets/images/avatar.jpg" alt="">
                 </el-tab-pane>
-                <el-tab-pane label="粉丝画像" name="charts">粉丝画像</el-tab-pane>
+                <el-tab-pane label="粉丝画像" name="charts">
+                    <div ref="bar" style="width: 600px; height: 400px"></div>
+                </el-tab-pane>
             </el-tabs>
         </el-card>
     </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
   data () {
     return {
       activeName: 'list'
     }
+  },
+  mounted () { // 组件渲染后的钩子函数
+    const dom = this.$refs.bar // 获取dom
+    const myEcharts = echarts.init(dom) // 初始化echarts实例
+    const option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
+    }
+
+    myEcharts.setOption(option)
   }
 }
 </script>
